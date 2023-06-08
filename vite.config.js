d79c2ac98947da7c9d72b9ae181e50de1cb03f75
Vnuk93 <vnuk93@example.com>
2023-06-08 23:08:25 +0200

    fix package for build

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,23 @@
-const path = require('path')
+const path = require("path");
 
 export default {
-  root: path.resolve(__dirname, 'src'),
+  root: path.resolve(__dirname, "src"),
   resolve: {
     alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-    }
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+    },
   },
   server: {
     port: 8080,
-    hot: true
-  }
-}
+    hot: true,
+  },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "src/index.html"),
+      },
+    },
+  },
+};
